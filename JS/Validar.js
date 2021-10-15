2c591347e8502ee72280b0f2f2e6c7044a45a11b
Escobitaaaaa <91977795+Escobitaaaaa@users.noreply.github.com>
2021-10-15 11:22:11 -0600

    Add files via upload

---
+++ JS/Validar.js
@@ -0,0 +1,13 @@
+function go(){
+    if (document.formingreso.usuario.value=='alex' && document.formingreso.contraseña.value=='examen')
+    {
+    window.location="html/Inicio.html"
+    }
+
+    else
+    {
+        alert("ingrese usuario y contraseña correcta")
+    }
+
+
+}
